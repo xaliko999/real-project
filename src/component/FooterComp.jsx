4e3b7611afc8a893c808b7becc7cdf8a91904cf0
xaliko999 <xalikomasixullayev@gmail.com>
2025-05-01 18:34:25 +0500

    gg

--- src/component/FooterComp.jsx
+++ src/component/FooterComp.jsx
@@ -10,9 +10,9 @@ function FooterComp() {
                 <p>+1234567890</p>
 
                 <div className="social-icons">
-                    <a href="t.me/mars_1140"><img src="./telegram.png" alt="Facebook" /></a>
-                    <a href="https://www.instagram.com/reel/DA0dwz8O4g5/?igsh=MWUzZ3BlZWhxemw0cA%3D%3D"><img src="./inst.png" alt="Instagram" /></a>
-                    <a href="https://youtu.be/Pjc3Bd9m7_k?si=QhK9arBFB6oTj82Y"><img src="./yotbe.png" alt="YouTube" /></a><br />
+                    <a href="t.me/mars_1140"><img src="telegram.png" alt="Facebook" /></a>
+                    <a href="https://www.instagram.com/reel/DA0dwz8O4g5/?igsh=MWUzZ3BlZWhxemw0cA%3D%3D"><img src="inst.png" alt="Instagram" /></a>
+                    <a href="https://youtu.be/Pjc3Bd9m7_k?si=QhK9arBFB6oTj82Y"><img src="yotbe.png" alt="YouTube" /></a><br />
                 </div> 
             </div>   
         </footer>
